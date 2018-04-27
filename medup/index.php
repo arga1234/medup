c02f09f8b202b51dc730b81cdf87c6947dec8ddc
arga1234 <33050211+arga1234@users.noreply.github.com>
2018-04-27 21:58:08 +0700

    Add files via upload

---
+++ medup/index.php
@@ -0,0 +1,375 @@
+<html>
+<head>
+  <!-- META DATA -->
+	<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="Free Bootstrap Themes by Zerotheme dot com - Free Responsive Html5 Templates">
+    <meta name="author" content="https://www.Zerotheme.com">
+
+	<title>Medup Library</title>
+
+	<!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css"  type="text/css">
+
+	<!-- Owl Carousel Assets -->
+    <link href="owl-carousel/owl.carousel.css" rel="stylesheet">
+    <!-- <link href="owl-carousel/owl.theme.css" rel="stylesheet"> -->
+
+	<!-- Custom CSS -->
+    <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.min.css">
+
+	<!-- Custom Fonts -->
+    <link rel="stylesheet" href="font-awesome-4.4.0/css/font-awesome.min.css"  type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Asap:400,700' rel='stylesheet' type='text/css'>
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+        <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+ <body>
+<header>
+
+	<!-- /////////////////////////////////////////Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <h1 class="navbar-brand page-scroll">
+					<a href="#page-top">MEDUP LIBRARY</a>
+				</h1>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="about.html">Tentang Kami</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="gallery.html">Buku Kuliah</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="service.html">Buku Bisnis</a>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+	<!-- Navigation -->
+	<div class="box-shadow">
+		<!-- Carousel -->
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/perpustakaan.jpg " style="width:100%">
+      <div class="carousel-caption">
+        <h3 style="padding-bottom:15%;padding-top:15%">WELCOME</h3>
+
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="images/perpustakaan.jpg"  style="width:100%">
+      <div class="carousel-caption">
+        <h3 style="padding-bottom:15%;padding-top:15%">MEDUP LIBRARY</h3>
+
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+</header>
+
+
+
+<section class="box-content box-1 box-style-1" id="about" >
+			<div class="container">
+				<div class="row heading">
+					 <div class="col-lg-12">
+	                    <h2></h2>
+
+	                </div>
+				</div>
+				<div class="row" style="margin-top:50px">
+					<div class="col-sm-6">
+						<div class="box-item">
+							<div class="wrap-img" style="width: 212px;">
+								<img src="images/perpus.png">
+							</div>
+							<h4 class="services-heading">Medup Library</h4>
+							<p style="margin-left:50px ;margin-right:50px;">Medup Library adalah platform perpustakaan online yang dapat anda akses untuk mengelola, melihat, atau meminjam buku secara lebih mudah dan modern</p>
+							<div class="btn btn-primary">MORE</div>
+						</div>
+			 		</div>
+			 		<div class="col-sm-6">
+							<div class="box-item">
+							<div class="wrap-img" style="width: 300px;">
+								<img src="images/onlinebook.jpg" />
+							</div>
+							<h4 class="services-heading">Ingin Booking Buku Online ?</h4>
+							<p style="margin-left:50px ;margin-right:50px;">Medup Library menyediakan fitur pinjam buku via online yang memudahkan anda untuk mendaftarkan diri sekaligus meminjam buku yang anda inginkan</p>
+							<div class="btn btn-primary">MORE</div>
+						</div>
+						</div>
+			 		</div>
+				</div>
+			</div>
+		</section>
+
+
+		<!-- ////////////Content Box Blog -->
+
+		<section class="box-content box-2" id="services">
+			<div class="container" style="margin-bottom: -20px">
+				<div class="row heading">
+					 <div class="col-lg-12">
+	                    <h2>Buku Favorit </h2>
+	                    <hr>
+	                </div>
+				</div>
+				<br><br>
+				<div class="row">
+					<div class="portfolio-items">
+						<div class="col-sm-6 col-md-4 col-lg-4 taupo_central">
+							<div class="post">
+								<div class="item-container wow fadeInUp" data-wow-delay="200ms">
+									<div class="item-caption">
+										<div class="item-caption-inner">
+											<div class="item-caption-inner1">
+												<a class="example-image-link" href="images/buku1.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+													<i class="fa fa-search"></i>
+												</a>
+											</div>
+										</div>
+									</div>
+									<img src="images/buku1.jpg" />
+								</div>
+								<h5>Buku Pertama</h5>
+								<span>Halo pembaca Medup Library ini adalah buku favorit yang ketiga</span>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-4 col-lg-4 taupo_central">
+						  <div class="post">
+								<div class="item-container wow fadeInUp" data-wow-delay="200ms">
+									<div class="item-caption">
+										<div class="item-caption-inner">
+											<div class="item-caption-inner1">
+												<a class="example-image-link" href="images/buku2.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+													<i class="fa fa-search"></i>
+												</a>
+											</div>
+										</div>
+									</div>
+									<img src="images/buku2.jpg" />
+								</div>
+								<h5>Buku Kedua</h5>
+								<span>Halo pembaca Medup Library ini adalah buku favorit yang kedua</span>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-4 col-lg-4 taupo_central">
+							<div class="post">
+								<div class="item-container wow fadeInUp" data-wow-delay="200ms">
+									<div class="item-caption">
+										<div class="item-caption-inner">
+											<div class="item-caption-inner1">
+												<a class="example-image-link" href="images/buku3.jpg" data-lightbox="example-set" data-title="Click the right half of the image to move forward.">
+													<i class="fa fa-search"></i>
+												</a>
+											</div>
+										</div>
+									</div>
+									<img src="images/buku3.jpg" />
+								</div>
+								<h5>Buku Ketiga</h5>
+								<span>Halo pembaca Medup Library ini adalah buku favorit yang ketiga.</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+		<!-- ////////////Table peminjam buku-->
+			<section class="box-content box-4 box-style-1" >
+				<div class="container">
+
+					<h4 style="text-align:center;">Peminjam Buku Favorit</h4>
+  				<hr>
+				<br><br>
+
+				<table  class="table table-striped ">
+	  		<thead >
+					<tr>
+						<th scope="col">No</th>
+						<th scope="col">NIM</th>
+						<th scope="col">Nama Peminjam</th>
+						<th scope="col">Asal Universitas</th>
+						<th scope="col">Judul Buku</th>
+
+					</tr>
+				</thead>
+					<?php
+					include "koneksi.php";
+					$sql = $pdo->prepare("SELECT * FROM peminjam_buku");
+					$sql->execute(); // Eksekusi querynya
+					$no = 1;
+					while($data = $sql->fetch()){
+						echo "<tr>";
+						echo "<td>".$no."</td>";
+						echo "<td>".$data['nim']."</td>";
+						echo "<td>".$data['nama_peminjam']."</td>";
+						echo "<td>".$data['asal_universitas']."</td>";
+						echo "<td>".$data['judul_buku']."</td>";
+
+						echo "</tr>";
+
+						$no++; // Tambah 1 setiap kali looping
+					}
+					?>
+				</table>
+			</div>
+		</section>
+
+
+
+
+		<!-- ////////////Find Us -->
+		<section class="box-content box-4 box-style-1" id="news">
+			<div class="container">
+				<div class="row heading">
+					 <div class="col-lg-12">
+	                    <h2>Find Us</h2>
+	                    <hr>
+	                </div>
+				</div>
+					<br><br>
+				<div class="row">
+					<div class="col-md-6" id="map" style="width:100%;height:400px">
+					</div>
+					<div class="col-md-6" style="margin-top: 30px;">
+
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+
+	<!--GMAPS-->
+	<script>
+		function myMap() {
+		  var mapCanvas = document.getElementById("map");
+		  var myCenter = new google.maps.LatLng(-7.772690, 110.375675);
+		  var mapOptions = {center: myCenter, zoom: 17};
+		  var map = new google.maps.Map(mapCanvas,mapOptions);
+		  var marker = new google.maps.Marker({
+		    position: myCenter,
+		    animation: google.maps.Animation.BOUNCE
+		  });
+		  marker.setMap(map);
+		}
+	</script>
+
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB72Tfu7t09nhfL3qNUV-y_tLFGTHFjzPU&callback=myMap"></script>
+
+	<!-- FOOTER -->
+	<footer>
+		<div class="wrap-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-footer col-md-4">
+						<h2 class="footer-title">Alamat</h2>
+						<div class="textwidget">UGM YK,Sinduadi, Mlati, Sleman Regency, Special Region of Yogyakarta 12345</div>
+					</div>
+					<div class="col-footer col-md-4">
+						<h2 class="footer-title">Kontak</h2>
+						<div class="textwidget">087836150131</div>
+					</div>
+					<div class="col-footer col-md-2">
+						<h2 class="footer-title">Email</h2>
+						<div class="textwidget">medup.official@gmail.com</div>
+					</div>
+					</div>
+
+				</div>
+			</div>
+
+	</footer>
+
+	<!-- jQuery -->
+	<script type="text/javascript" src="js/jquery-2.1.1.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+	<!-- Custom Theme JavaScript -->
+	<script src="js/agency.js"></script>
+
+	<!-- Plugin JavaScript -->
+	<script src="js/jquery.easing.min.js"></script>
+	<script src="js/classie.js"></script>
+	<script src="js/cbpAnimatedHeader.js"></script>
+
+	<script type="text/javascript" src="js/wow.min.js"></script>
+	<script type="text/javascript" src="js/jquery.isotope.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+
+
+	<!-- carousel -->
+	<script src="owl-carousel/owl.carousel.js"></script>
+    <script>
+    $(document).ready(function() {
+      $("#owl-brand").owlCarousel({
+        autoPlay: 3000,
+        items : 6,
+		itemsDesktop : [1199,4],
+        itemsDesktopSmall : [979,2],
+		navigation: true,
+		navigationText: ['<i class="fa fa-chevron-left fa-5x"></i>', '<i class="fa fa-chevron-right fa-5x"></i>'],
+		pagination: false
+      });
+    });
+    </script>
+
+</body>
+</html>
